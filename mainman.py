618fed4f5bfa027c06fb3750b8be1d47b331b3bd
Akshit Tyagi <tyagiakshit833@gmail.com>
2022-08-14 03:57:03 +0530

    implement api :')

--- mainman.py
+++ mainman.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+from fastapi import FastAPI, Path
+
+pyt = FastAPI()
+
+@pyt.get("/{search_term}")
+def get_info(search_term: str):
+    return main(search_term)
+
 def main(search_term):
     resp = requests.get(f"https://vid.puffyan.us/search?q={search_term}")
     txt = resp.text
@@ -10,14 +18,27 @@ def main(search_term):
     n = len(l)
 
     i = 0
+    m = 0
+    dic = {}
+
     while i < n:
         a = l[i]
         if str(a).find('class') == -1:
-            print("\n ", l[i].getText())
+            title = l[i].getText()
         elif str(a).find('class="channel-name"') != -1:
-            print(' |', l[i].getText())
-        elif (str(a).find('class="video-data"') != -1):
-            print(' - ', l[i].getText())
+            channel = l[i].getText()
+        elif str(a).find('ago') != -1:
+            t_upload = l[i].getText()
+        elif str(a).find('views') != -1:
+            views = l[i].getText()
+            dic[m] = {
+                "title" : title,
+                "channel" : channel,
+                "upload_time" : t_upload,
+                "views" : views
+                }
+            m += 1  
         i += 1
-main('pokemon')
-    
+    print(dic)
+    return dic
+
