9dbec9d06564cd08f506b7da293cf4121ac597cb
Akshit Tyagi <tyagiakshit833@gmail.com>
2022-08-14 03:16:24 +0530

    python function

--- mainman.py
+++ mainman.py
@@ -1,37 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-resp = requests.get("https://vid.puffyan.us/search?q=pokemon")
-txt = resp.text
+def main(search_term):
+    resp = requests.get(f"https://vid.puffyan.us/search?q={search_term}")
+    txt = resp.text
 
-soup = BeautifulSoup(txt, "html.parser")
-l = soup.find_all(attrs={'dir':'auto'})
-n = len(l)
+    soup = BeautifulSoup(txt, "html.parser")
+    l = soup.find_all(attrs={'dir':'auto'})
+    n = len(l)
 
-i = 0
-while i < n:
-    # try:
-    #     if l[i]['class'][0] == 'channel-name':
-    #         print(' | ',l[i].getText())
-    #     i += 1
-    # except:
-    #     try:
-    #         if l[i]['class'][0] == 'video-data':
-    #             print(' - ', l[i].getText())
-    #         i+=1
-    #     except:
-    #         i+=1
-    a = l[i]
-    if str(a).find('class="channel-name"') != -1:
-        print(' |', l[i].getText())
+    i = 0
+    while i < n:
+        a = l[i]
+        if str(a).find('class') == -1:
+            print("\n ", l[i].getText())
+        elif str(a).find('class="channel-name"') != -1:
+            print(' |', l[i].getText())
+        elif (str(a).find('class="video-data"') != -1):
+            print(' - ', l[i].getText())
         i += 1
-    elif (str(a).find('class="video-data"') != -1):
-        print(' - ', l[i].getText())
-    else:
-        print("\n ", l[i].getText())
-    i += 1
-
-# print(l[145]['class'][0])
-# print(l[145])
-# if l[145]['class'][0]=='channel-name':
-#     print(l[145].getText())
+main('pokemon')
+    
