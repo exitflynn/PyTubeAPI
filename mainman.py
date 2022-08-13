6848b0497c10e7cf85405f4d0a32bfa192c65621
Akshit Tyagi <tyagiakshit833@gmail.com>
2022-08-14 02:38:00 +0530

    everything was going nice

--- mainman.py
+++ mainman.py
@@ -9,28 +9,28 @@ l = soup.find_all(attrs={'dir':'auto'})
 n = len(l)
 
 i = 0
-# while i < n:
-#     try: 
-#         data = l[i]
-    # if data['class'][0]=='channel-name':
-    #     print(data.getText())
-        # if (data['class']) == ['channel-name']:
-        #     print(data.getText())
-        #     i += 1
-    # except:
-        # i += 1
 while i < n:
-    try:
-        if l[i]['class'][0] == 'channel-name':
-            print(' | ',l[i].getText())
+    # try:
+    #     if l[i]['class'][0] == 'channel-name':
+    #         print(' | ',l[i].getText())
+    #     i += 1
+    # except:
+    #     try:
+    #         if l[i]['class'][0] == 'video-data':
+    #             print(' - ', l[i].getText())
+    #         i+=1
+    #     except:
+    #         i+=1
+    a = l[i]
+    if str(a).find('class="channel-name"') != -1:
+        print(' |', l[i].getText())
         i += 1
-    except:
-        try:
-            if l[i]['class'][0] == 'video-data':
-                print(' - ', l[i].getText())
-            i+=1
-        except:
-            i+=1
+    else if str(a).find('class="video-data"') != -1:
+        print(' - ', l[i].getText())
+    else:
+        print("\n ", ;[i].getText())
+    i += 1
+
 # print(l[145]['class'][0])
 # print(l[145])
 # if l[145]['class'][0]=='channel-name':
